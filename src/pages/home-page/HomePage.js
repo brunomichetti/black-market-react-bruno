69d1509664d21c591b70fe62194ca5f209b2701b
Bruno Michetti <brunomichetti@rootstrap.com>
2022-07-22 13:08:19 -0300

    Fixes suggestions

--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -2,17 +2,15 @@ import React from "react";
 
 import "./home-page.scss";
 
-const HomePage = () => {
-  return (
-    <div className="home-page">
-      <div className="signup">
-        <p className="regular-text">Don't have an account?</p>
-        <button type="button" className="common-white-button">
-          Sign up
-        </button>
-      </div>
+const HomePage = () => (
+  <div className="home-page">
+    <div className="signup">
+      <p className="regular-text">Don't have an account?</p>
+      <button type="button" className="common-white-button regular-text">
+        Sign up
+      </button>
     </div>
-  );
-};
+  </div>
+);
 
 export default HomePage;
